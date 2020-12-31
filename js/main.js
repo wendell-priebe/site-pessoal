4b49fcef99593434eba0e7966e6e2682ab050acd
Wendell Priebe <wendel_oliveira94@hotmail.com>
2020-12-31 15:04:24 -0300

    cookies and button portfolio

--- js/main.js
+++ js/main.js
@@ -6,10 +6,54 @@ const
     estilo = document.getElementById("estilo"),
     button_dark = document.getElementById("button");
 
+permanencCookie()
 
 // ATIVAR MODO DARK
-button_dark.addEventListener("click", ()=>{
+function darkMode(){
+    // ativa o modo dark
+    onOffDark()
 
+    // verifica se esta true/false e troca estado cookie
+    let resCookie = checkCookie("cookie_valid")
+    if (resCookie) {
+        creatCookie("cookie_valid", false, 30)
+    }else{
+        creatCookie("cookie_valid", true, 30)
+    }
+
+}
+
+// verifica estado true/false cookie
+function checkCookie (name){
+    // cria o cookie caso nao exista
+    let testname = document.cookie.includes(name)
+
+    if (!testname) {
+        creatCookie("cookie_valid", false, 30)
+        return testname
+    }
+    // retorna o valor de cookie
+    let [,test] = document.cookie.split("=")
+    if (test==="true") {
+        return true
+    }else{
+        return false
+    }
+}
+// quando o cookie estiver true liga o modo dark
+function permanencCookie(){
+    let resCookie = checkCookie("cookie_valid")
+    if (resCookie) {
+        onOffDark()
+    }
+}
+// cria cookie
+function creatCookie(name, value, date){
+    let dateExpires = "expires="+date
+    document.cookie = name + "=" + value + "; " + dateExpires;
+}
+// aplica as alteraçoes no html
+function onOffDark(){
     button_dark.classList.toggle('button_active')
 
     if (button_dark.classList.contains("button_active")) {
@@ -17,9 +61,7 @@ button_dark.addEventListener("click", ()=>{
     } else {
         estilo.href = "css/white.css"
     }
-
-
-})
+}
 
 
 // MENU LATERAL
@@ -32,13 +74,10 @@ closeMenu.addEventListener(`click`, ()=>{
     navMenu.classList.remove(`show`)
 } )
 
-    // ANIMATION
+
+// ANIMATION
 gsap.from(`.nav_logo, .nav_toggle`, {opacity:0, duration:1, delay:2, y:-10})
 gsap.from(`.nav_item`, {opacity:0, duration:1, delay:2.5, y:30, stagger:0.2})
 gsap.from(`.big_logo`, {opacity:0, duration:1.5, delay:1, y:30, stagger:0.2})
 gsap.from(`.about`, {opacity:0, duration:1.5, delay:2, y:30, stagger:0.2})
 gsap.from(`.skills`, {opacity:0, duration:1.5, delay:2.5, y:30, stagger:0.2})
-
-
-
-
